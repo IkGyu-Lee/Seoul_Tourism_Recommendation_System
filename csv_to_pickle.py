93626e722f4c8528e2fa544b8ba8ff8f9abdf711
IkGyu-Lee <dlrrb0521@gmail.com>
2022-02-17 18:00:26 +0900

    dataset change from csv to pickle

---
+++ csv_to_pickle.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+
+ROOT_DIR = 'dataset'
+DEST_INFO_PATH = os.path.join(ROOT_DIR, 'destination_id_name_genre_coordinate.csv')
+PREICTED_CONGEST_PATH = os.path.join(ROOT_DIR, 'congestion_1_2.csv')
+CITY_INFO_PATH = os.path.join(ROOT_DIR, 'seoul_gu_dong_coordinate.csv')
+
+li = [DEST_INFO_PATH,PREICTED_CONGEST_PATH,CITY_INFO_PATH]
+for i in li:
+    df = pd.read_csv(i)
+    df.to_pickle(i.rstrip('csv')+'pkl')
